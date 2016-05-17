96dedcaa5cce786ba2d82d997f2960ff70178c01
Anthony Harris <aharris12@metlife.com>
2016-05-17 10:50:16 -0400

    Scroll More

--- dist/js/metlife.js
+++ dist/js/metlife.js
@@ -1365,41 +1365,25 @@ function scrollMoreBouncing() {
     });
 }
 
+
 function scrollForMoreFunction() {
-        if ($(".scroll-form-more-container").length != 0){
+    if ($(".scroll-form-more-container").length != 0) {
         $(window).scroll(function () {
             var height = $(".global_header").height(),
-                carsouselHeight = $("#carouselHero").height(),
-                scrollerlHeight = $(".scroll-form-more-container").height(),
                 doc = document.documentElement,
-                top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+                top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0),
+                scrollVisible = $(".scroll-form-more-container").css("display");
 
-            if ($(".hidden-sm, .hidden-xs").is(":visible") && top >= (carsouselHeight + height + scrollerlHeight + 200)) {
-                if ($(".scroll-form-more-container").css("display") != "none") {
-                    $('html, body').animate({scrollTop: $(".divider--snoopy").offset().top - height - 30}, 500);
-                    $(".scroll-form-more-container").hide();
-                }
+            if ($(".hidden-sm, .hidden-xs").is(":visible") && top > 50 && scrollVisible != "none") {
+
+                $('html, body').animate({scrollTop: $(".divider--snoopy").offset().top - height - 30}, 500);
+                $(".scroll-form-more-container").hide();
+                runOnce = false;
             }
 
         });
     }
 }
-
-/*
-function scrollForMoreFunction() {
-    if ($("#carouselHero").length != 0 && $(".scroll-form-more-container").length != 0) {
-        if ($(".hidden-sm").is(":visible") && $(".hidden-xs").is(":visible")) {
-            $(window).scroll(function () {
-                var height = $(".global_header").height();
-                if ($(window).scroll(200) && $(".scroll-form-more-container").css("display") != "none") {
-                    $('html, body').animate({scrollTop: $(".divider--snoopy").offset().top - height - 30}, 500);
-                    $(".scroll-form-more-container").hide();
-                }
-            });
-        }
-    }
-}
-*/
 
 
 $("#lifeStages .carousel-item").click(function () {
