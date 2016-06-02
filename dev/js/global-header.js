ffd8d67ffe49ef65342913c999fe3c9bf506900c
crestino <crestino@metlife.com>
2016-06-02 15:58:21 -0400

    Fix the search input not closing when clicking off element

--- dev/js/global-header.js
+++ dev/js/global-header.js
@@ -79,13 +79,8 @@ function resizeMegaMenu () {
 
 }
 
-$("body").on("click tap", function (e) {
-    var megaMenuTrigger = $('.megamenu-trigger');
-    var container = $(".search-trigger");
-    if (!container.is(e.target) && container.has(e.target).length === 0 && !megaMenuTrigger) {
-        closeSearchBox();
-    }
-});
+
+
 
 
 function openSearchBox () {
@@ -112,6 +107,15 @@ function closeSearchBox () {
     $('.search-trigger__icon').removeClass('search-trigger__icon--open');
     $('.search-trigger__search-box').css('width',"0");
 }
+
+$(document).on("click tap", function (e) {
+    var megaMenuTrigger = $(".megamenu-trigger");
+    var container = $(".search-trigger");
+    if (!container.is(e.target) && container.has(e.target).length === 0 &&!megaMenuTrigger.is(e.target) && megaMenuTrigger.has(e.target).length==0) {
+        closeSearchBox();
+    }
+});
+
 
 $('.megamenu-trigger').on('click', function(){
     $('.' + $(this).attr('data-target')).toggleClass('megamenu--open');
