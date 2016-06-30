8a7d8d1286c3c882e17182349056ee1a7028da63
Ian Cunningham <imcunningham88@gmail.com>
2016-06-30 11:51:11 -0400

    Country selector fix and tooltip
    Country selector and tooltip addition

--- dev/js/global-footer.js
+++ dev/js/global-footer.js
@@ -4,14 +4,45 @@ $(document).ready(function(){
 });
 
 
+$.fn.isOnScreen = function(){
+    var win = $(window);
+    var viewport = {
+        top  : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+
+var marketingCarouselVisible;
+
+$(window).scroll(function(){
+    if ($('#countryList').isOnScreen() == false) {
+        closeCountryList();
+    }
+});
+
+
+$(document).ready(function(){
+    footerBorder();
+});
+
+
 function closeCountryList() {
     $('.country__list').slideUp(200).scrollTop(0);
 }
 
 function processCountrySelection(evt) {
-    var countrySelectActivationClasses = ['countryNameSelected','countryFlagSelected','countrySelected','countrySVG', 'country__label'];
-    if (evt.target.id == "" || evt.target.id == "countryList" || evt.target.className == "country_continent" ) {
 
+    var countrySelectActivationClasses = ['countryNameSelected','countryFlagSelected','countrySelected','countrySVG'];
+    if (evt.target.id == "" || evt.target.id == "countryList" || evt.target.className == "country_continent") {
         //evt.stopPropagation();
         closeCountryList();
         return;
@@ -34,26 +65,58 @@ function processCountrySelection(evt) {
     }
 }
 
+/*
+ $('body').on('touchstart', function(e) {
+ processCountrySelection(e);
+ });
+ */
+
 var clickDisabled = false;
 
 
 $('body').on ('click touchstart', function(e){
+
     if (clickDisabled != true){
+
         var clickEvent = ((document.ontouchstart!==null)?'click':'touchstart');
+
         switch(clickEvent) {
             case 'click':
                 processCountrySelection(e);
                 break;
             case 'touchstart':
-                processCountrySelection(e);
+
+                //var cs = document.getElementById(("countryList");
+
+                if($("#countryList").is(":visible") == true){
+                    //if(e.target.id !="countrySelected") closeCountryList();
+                }else{
+                    processCountrySelection(e);
+                }
+
                 break;
             default:
                 break;
         }
+
         clickDisabled = true;
         setTimeout(function(){clickDisabled = false;}, 1000);
     }
+
 });
+
+
+
+/*
+ When disclaimer is not present, remove top-border from footer
+ */
+
+function footerBorder(){
+    if ($(".disclaimer--main").length == 0){
+        $(".global-footer .wrapper").css("border-top", "none");
+        $(".global-footer .wrapper").css("padding-top", "0");
+    }
+}
 
 
 
