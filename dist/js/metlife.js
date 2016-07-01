93f96b2d5a24153ad9481d83a9c6d98046ae8d7d
Anthony Harris <anthonyharris721@gmail.com>
2016-07-01 10:59:53 -0400

    Grunt

--- dist/js/metlife.js
+++ dist/js/metlife.js
@@ -27826,10 +27826,10 @@ $(document).ready(function(){
 
 function removePaddingWrapper(){
 	var container = $(".container.contextual-links");
-	console.log(container.length)
+
 console.log(container.length > 0)
 	if(container.length > 0){
-		console.log(container.next(".container"))
+
 		container.next(".container").find(".wrapper").css("padding-top", "0px");
 	}
 }
