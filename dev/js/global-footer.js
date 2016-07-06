4c79c625b0b5d1db94e0467f91ed2aecd788b302
Alex Williams <awilliams27@metlife.com>
2016-07-06 09:18:00 -0400

    removed function that was failing on other pages ($.fn.isOnScreen) - looked like it was unnecessary

--- dev/js/global-footer.js
+++ dev/js/global-footer.js
@@ -3,30 +3,10 @@ $(document).ready(function(){
     footerBorder();
 });
 
-
-$.fn.isOnScreen = function(){
-    var win = $(window);
-    var viewport = {
-        top  : win.scrollTop(),
-        left : win.scrollLeft()
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
-
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
-
-    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-
-};
-
 var marketingCarouselVisible;
 
 $(window).scroll(function(){
-    if ($('#countryList').isOnScreen() == false) {
-        closeCountryList();
-    }
+    closeCountryList();
 });
 
 
