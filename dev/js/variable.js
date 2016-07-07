380ad4b90c67ae9f05484fcff4ccdb3087acacaa
Ian Cunningham <imcunningham88@gmail.com>
2016-07-07 15:31:24 -0400

    added console.log

--- dev/js/variable.js
+++ dev/js/variable.js
@@ -8,8 +8,16 @@ var breakpointTabletOverlay = 768;
 var breakpointDesktopOverlay = 1023;
 var imagesPath = "";
 
+//Production code
 if ( localStorage.getItem("contextPath") ) {
     imagesPath = localStorage.getItem("contextPath") + "/static/images/";
 } else {
     imagesPath = "/static/images/";
 }
+
+//Local testing code
+//if ( localStorage.getItem("contextPath") ) {
+//    imagesPath = localStorage.getItem("contextPath") + "/images/";
+//} else {
+//    imagesPath = "/images/";
+//}
