2deaebfa0a518de076ae8501ea425d390227efec
Anthony Harris <anthonyharris721@gmail.com>
2016-07-12 13:31:14 -0400

    UX fixes

--- dev/js/contextual--links.js
+++ dev/js/contextual--links.js
@@ -1,12 +1,43 @@
 $(document).ready(function(){
-	removePaddingWrapper();
+	removingPaddingContextualLinksContactForm();
+	removingPaddingContextualLinksProductTiles();
+	removingPaddingContextualLinksSmallCards()
 });
 
-function removePaddingWrapper(){
+function removingPaddingContextualLinksContactForm(){
 	var container = $(".container.contextual-links");
-
 	if(container.length > 0){
+		var thisContainer = container.next("div");
+		if(thisContainer.hasClass("contact-advisory")){
+			thisContainer.find(".container").css("cssText", "padding-top: 0px !important;");
+			thisContainer.find(".container").find(".wrapper").css("cssText", "padding-top: 0px !important;");
+		}
+	}
+}
 
-		container.next(".container").find(".wrapper").css("padding-top", "0px");
+function removingPaddingContextualLinksProductTiles(){
+	var container = $(".container.contextual-links");
+	if(container.length > 0){
+		var thisContainer = container.parent().prev().find(".product-card-parsys");
+		var prevContainer = container.prev("div");
+		if(thisContainer.length > 0 && prevContainer.length == 0){
+			thisContainer.find(".tile-container").last().find(".wrapper").css("cssText", "margin-bottom: -10px !important;");
+			thisContainer.last(".tile-container").find(".wrapper").find(".product-row__tile").each(function(){
+				$(this).css("cssText", "margin-bottom: 0px !important;");
+			});
+			thisContainer.last("tile-container").find(".single-promo").css("cssText", "margin-bottom: 0px !important;");
+			thisContainer.last("tile-container").find(".double-promo").css("cssText", "margin-bottom: 0px !important;");
+			thisContainer.last("tile-container").find(".triple-promo").css("cssText", "margin-bottom: 0px !important;");
+		}
+	}
+}
+
+function removingPaddingContextualLinksSmallCards(){
+	var container = $(".container.contextual-links");
+	if(container.length > 0){
+		var thisContainer = container.prev("div");
+		if(thisContainer.hasClass("small-product-container")){
+			thisContainer.find(".wrapper ").css("cssText", "margin-bottom: 0px !important;");
+		}
 	}
 }
