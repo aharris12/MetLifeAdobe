bb066bb654aff5aec0684486928a5d0dfd48d3d5
Anthony Harris <anthonyharris721@gmail.com>
2016-07-19 11:28:36 -0400

    Grunt

--- dist/js/metlife.js
+++ dist/js/metlife.js
@@ -5945,7 +5945,7 @@ $("[data-request-type]").on("change", function () {
 		}
 	});
 	if (thisValue != "") {
-		$("[data-request-type]").removeClass('error');
+		$(this).removeClass('error');
 		$(this).attr('data-valid-status', 'success');
 		$(this).parent('.form-user-grp').find('svg').css('fill', '#666');
 	}
