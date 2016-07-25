e908e2c02057c78f214c57935ef5211a437f4779
Anthony Harris <anthonyharris721@gmail.com>
2016-07-25 09:00:15 -0400

    Set up project for new hpome page and country specific css files. added new images. created new usglobal.html, modified grunt.

---
+++ dist/dev/js/cta-box.js
@@ -0,0 +1,27 @@
+
+$(window).load(function () {
+    formatCTABoxes();
+});
+
+$(window).resize(function (e) {
+    formatCTABoxes();
+});
+
+
+
+function formatCTABoxes() {
+
+    if($(".cta-box-module").length!=0){
+        $(".cta-box-module").each(function () {
+            var layout = $(this);
+            var number = layout.children(".cta-box").length;
+            if (number <= 4) {
+                layout.addClass("large");
+            } else {
+                layout.addClass("small");
+            }
+            layout.show();
+        });
+    }
+
+}
