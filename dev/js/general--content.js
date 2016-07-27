58f2ef13a8c151009fe953ab149cb8401a9d3c6f
Anthony Harris <anthonyharris721@gmail.com>
2016-07-27 11:40:52 -0400

    Added saftely feature

--- dev/js/general--content.js
+++ dev/js/general--content.js
@@ -1,5 +1,5 @@
 $(".generic_content .expand_button").click(function () {
-	$(".unexpanded").slideToggle();
+	$(this).parent(".generic_content").find(".wrapper-general").find(".unexpanded").slideToggle();
 	$(this).find(".expand_button_open").toggleClass("hidden");
 	$(this).find(".expand_button_close").toggleClass("hidden");
 	if ($(".rate_table").length > 0) {
