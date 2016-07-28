8d90bd5d6b9e3cb94508f4743e10b5c1887a89b6
Anthony Harris <anthonyharris721@gmail.com>
2016-07-28 12:31:26 -0400

    Changed specs

--- dev/js/email.js
+++ dev/js/email.js
@@ -8,7 +8,7 @@ $(window).resize(function(){
 });
 
 function emailInputSize(){
-	if ($(".hidden-xs").is(":visible") && $(".email--unsubscribe__form-fields").length > 0) {
+	if (getViewport() != "mobile" && $(".email--unsubscribe__form-fields").length > 0) {
 		var minusWidth = $(".email--unsubscribe__form-fields button")[0].getBoundingClientRect().width;
 		var calcWidth = (minusWidth + 10).toFixed(2);
 		$(".email--unsubscribe__form-fields input").css("width", "Calc(100% - " + calcWidth + "px" + ")");
