0d2be6901851778f376d0cd81d68a3ebf71b3e9c
Anthony Harris <anthonyharris721@gmail.com>
2016-08-04 13:53:06 -0400

    Grunt

--- dist/js/metlife.js
+++ dist/js/metlife.js
@@ -891,12 +891,12 @@ $( document ).ready(function() {
     //var carouselCaptionPaddingBottom = 100;
     $.lazyLoadXT.autoLoadTime = lazyPause;
     //Adjust carousel-caption container's height
-    $.lazyLoadXT.onload = function() {
+   /* $.lazyLoadXT.onload = function() {
         $('.carousel .carousel-caption--hero').innerHeight($('.carousel').height());
-    };
+    };*/
 
 
-    $('.carousel .carousel-caption--hero').innerHeight($('.carousel').height());
+    /*$('.carousel .carousel-caption--hero').innerHeight($('.carousel').height());*/
 
     //Reloadoad images on resize
     var resizeTimeout;
@@ -907,7 +907,7 @@ $( document ).ready(function() {
         clearTimeout(resizeTimeout);
       });
     $( window ).resize(function() {
-        $('.carousel .carousel-caption--hero').innerHeight($('.carousel').height());
+        //$('.carousel .carousel-caption--hero').innerHeight($('.carousel').height());
         resizeTimeout = setTimeout(function () {
             $(window).lazyLoadXT({
                 checkDuplicates: false
