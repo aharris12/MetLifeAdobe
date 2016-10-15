b9ab958b2c4f78ca77486f3a8d18b687e96787af
Ziefle, Jon <jziefle@metlife.com>
2016-10-15 19:23:41 -0400

    updated spacing overrides

--- dev/js/spacing.js
+++ dev/js/spacing.js
@@ -1,54 +1,69 @@
 var SpacingModule = (function () {
     var spacing = {
         "product-module-large": {
-            "first": "30 30 30",
-            "last": "20 20 20",
-            "product-module-medium": ["30 30 30", ""],
-            "product-module-small": ["30 30 30", ""],
-            "office-search": ["30 30 30", ""],
-            "promo-card": ["30 30 30", ""],
-            "disclaimer": ["30 30 30", ""]
+            "office-search": ["20 40 50", ""],
+            "contextual-links": ["0 0 0", ""]
         },
-        //"product-module-medium": "test",
-        //"product-module-small": "test",
-        //"office-search": "test",
-        //"promo-card": "test",
-        "disclaimer": {
-            "last": "50 50 50"
-        }
+        "product-module-medium": {
+            "office-search": ["20 40 50", ""],
+            "contextual-links": ["0 0 0", ""]
+        },
+        "product-module-small": {
+            "office-search": ["20 40 50", ""],
+            "contextual-links": ["0 0 0", ""]
+        },
+        "promo-card": {
+            "disclaimer": ["10 40 50", ""]
+        },
+        "product-comparison-chart": {
+            "first": "20 30 40"
+        },
+        "contextual-links": {
+            "faq-and-video": ["", "20 40 50"],
+            "skinny-product-tile": ["", "20 0 0"]
+        },
+        "skinny-product-tile": {
+            "office-search": ["20 40 50", ""]
+        },
+        "faq-and-video": {
+            "office-search": ["", "20 40 50"]
+        },
+        "product-tiles": {
+            "product-module-small": ["10 30 40", ""]
+        },
+        "disclaimer": {}
     };
-
 
     // Initializes Spacing
     function init() {
-
-    }
-
-    function applySpacing() {
         var components = $(".component[data-component]");
         var length = components.length;
 
-        for (var index = 0; index < length - 1; index++) {
+        for (var index = 0; index < length; index++) {
             var comp1, comp2, prop1, prop2;
             comp1 = components.eq(index);
             prop1 = comp1.attr("data-component");
-            comp2 = components.eq(index + 1);
-            prop2 = comp2.attr("data-component");
+
 
             // Apply TOP styling to first component
             if ((index == 0) && spacing.hasOwnProperty(prop1) && spacing[prop1].hasOwnProperty("first")) {
                 comp1.addClass(classFormat(spacing[prop1]["first"], "top"));
             }
 
-            // Apply styling between components (BOTTOM/TOP)
-            if (spacing.hasOwnProperty(prop1) && spacing[prop1].hasOwnProperty(prop2)) {
-                comp1.addClass(classFormat(spacing[prop1][prop2][0], "bottom"));
-                comp2.addClass(classFormat(spacing[prop1][prop2][1], "top"));
+            // Apply BOTTOM styling to last component
+            if ((index == length - 1) && spacing.hasOwnProperty(prop1) && spacing[prop1].hasOwnProperty("last")) {
+                comp1.addClass(classFormat(spacing[prop1]["last"], "bottom"));
             }
 
-            // Apply BOTTOM styling to last component
-            if ((index == length - 2) && spacing.hasOwnProperty(prop2) && spacing[prop2].hasOwnProperty("last")) {
-                comp2.addClass(classFormat(spacing[prop2]["last"], "bottom"));
+            // Apply styling between components (BOTTOM/TOP)
+            if (index < length - 1) {
+                comp2 = components.eq(index + 1);
+                prop2 = comp2.attr("data-component");
+
+                if (spacing.hasOwnProperty(prop1) && spacing[prop1].hasOwnProperty(prop2)) {
+                    comp1.addClass(classFormat(spacing[prop1][prop2][0], "bottom"));
+                    comp2.addClass(classFormat(spacing[prop1][prop2][1], "top"));
+                }
             }
         }
     }
@@ -58,21 +73,20 @@ var SpacingModule = (function () {
         var string = "";
         if (val != "") {
             var pad = val.split(" ");
-            if (pos == "top") {
-                string += "pad-t-sm-" + pad[0] + " pad-t-md-" + pad[1] + " pad-t-lg-" + pad[2];
-            } else if (pos == "bottom") {
-                string += "pad-b-sm-" + pad[0] + " pad-b-md-" + pad[1] + " pad-b-lg-" + pad[2];
+            if (pad.length == 3) {
+                if (pos == "top") {
+                    string += "pad-t-sm-" + pad[0] + " pad-t-md-" + pad[1] + " pad-t-lg-" + pad[2];
+                } else if (pos == "bottom") {
+                    string += "pad-b-sm-" + pad[0] + " pad-b-md-" + pad[1] + " pad-b-lg-" + pad[2];
+                }
             }
         }
 
         return string;
     }
 
-
     return {
-        init: init,
-        applySpacing: applySpacing
+        init: init
     };
 })();
 SpacingModule.init();
-SpacingModule.applySpacing();
