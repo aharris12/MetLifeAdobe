b8c09b43a6930a74b5ec022984289c2ae0ba2da6
Ziefle, Jon <jziefle@metlife.com>
2016-10-15 18:13:39 -0400

    spacing rules engine created!

--- dev/js/spacing.js
+++ dev/js/spacing.js
@@ -1,20 +1,20 @@
 var SpacingModule = (function () {
     var spacing = {
         "product-module-large": {
-            "first": "pml-above",
-            "product-module-medium": ["0 0 0", "1 1 1"],
-            "product-module-small": ["2 2 2", "3 3 3"],
-            "office-search": ["444", "555"],
-            "promo-card": ["666", "777"],
-            "disclaimer": ["888", "999"],
-            "last": "below"
+            "first": "30 30 30",
+            "last": "20 20 20",
+            "product-module-medium": ["30 30 30", ""],
+            "product-module-small": ["30 30 30", ""],
+            "office-search": ["30 30 30", ""],
+            "promo-card": ["30 30 30", ""],
+            "disclaimer": ["30 30 30", ""]
         },
         //"product-module-medium": "test",
         //"product-module-small": "test",
         //"office-search": "test",
         //"promo-card": "test",
         "disclaimer": {
-            "last": "disc-below"
+            "last": "50 50 50"
         }
     };
 
@@ -25,42 +25,47 @@ var SpacingModule = (function () {
     }
 
     function applySpacing() {
-        var first, second, last, component;
         var components = $(".component[data-component]");
-
-        //first
-        first = components.first();
-        component = first.attr("data-component");
-        if (spacing.hasOwnProperty(component) && spacing[component].hasOwnProperty("first")) {
-            first.addClass(spacing[component].first);
-        }
-
-        //middle
         var length = components.length;
-        components.each(function(index) {
+
+        for (var index = 0; index < length - 1; index++) {
             var comp1, comp2, prop1, prop2;
-            comp1 = $(this);
+            comp1 = components.eq(index);
             prop1 = comp1.attr("data-component");
             comp2 = components.eq(index + 1);
             prop2 = comp2.attr("data-component");
 
-            if (spacing.hasOwnProperty(prop1) && spacing[prop1].hasOwnProperty(prop2)) {
-                comp1.addClass(spacing[prop1][prop2][0]);
-                comp2.addClass(spacing[prop1][prop2][1]);
+            // Apply TOP styling to first component
+            if ((index == 0) && spacing.hasOwnProperty(prop1) && spacing[prop1].hasOwnProperty("first")) {
+                comp1.addClass(classFormat(spacing[prop1]["first"], "top"));
             }
-        });
 
+            // Apply styling between components (BOTTOM/TOP)
+            if (spacing.hasOwnProperty(prop1) && spacing[prop1].hasOwnProperty(prop2)) {
+                comp1.addClass(classFormat(spacing[prop1][prop2][0], "bottom"));
+                comp2.addClass(classFormat(spacing[prop1][prop2][1], "top"));
+            }
 
-        //for (var i = 1; i < components.length - 1; i++) {
-        //    console.log(components.eq(i).data("component"));
-        //}
-
-        //last
-        last = components.last();
-        component = last.data("component");
-        if (spacing.hasOwnProperty(component) && spacing[component].hasOwnProperty("last")) {
-            last.addClass(spacing[component].last);
+            // Apply BOTTOM styling to last component
+            if ((index == length - 2) && spacing.hasOwnProperty(prop2) && spacing[prop2].hasOwnProperty("last")) {
+                comp2.addClass(classFormat(spacing[prop2]["last"], "bottom"));
+            }
         }
+    }
+
+    // Formats the CSS class names
+    function classFormat(val, pos) {
+        var string = "";
+        if (val != "") {
+            var pad = val.split(" ");
+            if (pos == "top") {
+                string += "pad-t-sm-" + pad[0] + " pad-t-md-" + pad[1] + " pad-t-lg-" + pad[2];
+            } else if (pos == "bottom") {
+                string += "pad-b-sm-" + pad[0] + " pad-b-md-" + pad[1] + " pad-b-lg-" + pad[2];
+            }
+        }
+
+        return string;
     }
 
 
